97df36ac02b27b359380e31edeb7666c9bfed8af
Abdulaziz14 <37253581+Abdulaziz14@users.noreply.github.com>
2018-05-03 03:17:10 +0300

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,11 +1,13 @@
 <?php
 
-$servername = "localhost";
-$username= "abdulaziz14h";
-$password= "";
-$database = "c9";
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-$conn =  new mysqli ($servername,$username,$password,$database);
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
 
  if($conn -> connect_error){
      echo("Failed to connect to database");
